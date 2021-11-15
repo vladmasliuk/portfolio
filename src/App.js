92a978ef0f5a4ee7c450801b5c6a3dc9ae615cf6
Wladek <vladmasliuk@gmail.com>
2021-11-15 19:05:37 +0100

    create project schema

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+
+// router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// pages
+import homePage from './pages/homePage';
+import notFoundPage from './pages/notFoundPage';
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={homePage}/>
+        <Route path="" component={notFoundPage}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/homePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const homePage = () =>{
+    return(
+        <h1>Home page</h1>
+    )
+}
+
+export default homePage;
