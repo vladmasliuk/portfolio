ac3cfce7ee19fbd4a7c79b014da00073a954443f
Wladek <vladmasliuk@gmail.com>
2021-12-06 20:16:30 +0100

    change projects

--- src/projects.js
+++ src/projects.js
@@ -1,42 +1,5 @@
 const projects = [
     {
-        "id": "adbl",
-        "title": "adbl",
-        "desc": "Huge website with catalog of products, training order system and map of distributors.",
-        "img": "https://i.ibb.co/V2CBrc1/adbl-main.jpg",
-        "url": "https://adbl.eu/",
-        "features": [
-            {
-                "title": "Map",
-                "desc": "Map of distributors with filtering by country, voivodeship, city and type of store. Created with Google maps API.",
-                "img": "https://i.ibb.co/HH961sd/adbl-map.gif",
-            },
-            {
-                "title": "Calendar",
-                "desc": "Calendar with available training dates. After clicking, the user will be redirected to the purchase of the training, if date is available.",
-                "img": "https://i.ibb.co/9wGNBsX/adbl-calendar.gif",
-            },
-            {
-                "title": "Category",
-                "desc": "If there are 7 or less days left to the selected training and nobody has purchased it, the training will be automatically blocked and will not be available for purchase",
-                "img": "https://i.ibb.co/0YqXKNF/adbl-category.gif",
-            },
-            {
-                "title": "Cart",
-                "desc": "Promotional package with a price of 1PLN is automatically added to the training, which cannot be removed from the cart. If there are more than 7 days left to the training, then the price for the training and promotional package is 50% lower. However, if the user wants to pay 100%, he has that option and price will chage to 100%.",
-                "img": "https://i.ibb.co/23b9dMq/adbl-cart.gif",
-            },
-        ],
-        "tech":[
-            {
-                "name": "wordpress"
-            },
-            {
-                "name": "woocommerce"
-            },
-        ],
-    },
-    {
         "id": "lesesame",
         "title": "lesesame",
         "desc": "Simple and cleen online shop.",
@@ -90,6 +53,66 @@ const projects = [
         ],
     },
     {
+        "id": "adbl",
+        "title": "adbl",
+        "desc": "Huge website with catalog of products, training order system and map of distributors.",
+        "img": "https://i.ibb.co/V2CBrc1/adbl-main.jpg",
+        "url": "https://adbl.eu/",
+        "features": [
+            {
+                "title": "Map",
+                "desc": "Map of distributors with filtering by country, voivodeship, city and type of store. Created with Google maps API.",
+                "img": "https://i.ibb.co/HH961sd/adbl-map.gif",
+            },
+            {
+                "title": "Calendar",
+                "desc": "Calendar with available training dates. After clicking, the user will be redirected to the purchase of the training, if date is available.",
+                "img": "https://i.ibb.co/9wGNBsX/adbl-calendar.gif",
+            },
+            {
+                "title": "Category",
+                "desc": "If there are 7 or less days left to the selected training and nobody has purchased it, the training will be automatically blocked and will not be available for purchase",
+                "img": "https://i.ibb.co/0YqXKNF/adbl-category.gif",
+            },
+            {
+                "title": "Cart",
+                "desc": "Promotional package with a price of 1PLN is automatically added to the training, which cannot be removed from the cart. If there are more than 7 days left to the training, then the price for the training and promotional package is 50% lower. However, if the user wants to pay 100%, he has that option and price will chage to 100%.",
+                "img": "https://i.ibb.co/23b9dMq/adbl-cart.gif",
+            },
+        ],
+        "tech":[
+            {
+                "name": "wordpress"
+            },
+            {
+                "name": "woocommerce"
+            },
+        ],
+    },
+    {
+        "id": "audiopremium",
+        "title": "audio premium",
+        "desc": "Simple description website",
+        "img": "https://i.ibb.co/HH4QNt1/audio.jpg",
+        "url": "https://audio-premium.pl/",
+        "screens": [
+            {
+                "url": "https://i.ibb.co/NLj6f8C/audio.jpg",   
+            },
+            {
+                "url": "https://i.ibb.co/2hHRY2J/audio2.jpg",
+            },
+            {
+                "url": "https://i.ibb.co/S03mcPq/audio3.jpg",
+            },
+        ],
+        "tech":[
+            {
+                "name": "wordpress"
+            },
+        ],
+    },
+    {
         "id": "socialmedia",
         "title": "social media",
         "desc": "Simple social media with registration, login, ability to publish posts, commenting and liking them. User also can add and edit his profile data (photo, about and location). After liking or commenting on a post, the user will get a notification. From description website you can download the desktop or mobile version of application.",
@@ -131,29 +154,6 @@ const projects = [
             },
             {
                 "name": "Next JS"
-            },
-        ],
-    },
-    {
-        "id": "audiopremium",
-        "title": "audio premium",
-        "desc": "Simple description website",
-        "img": "https://i.ibb.co/HH4QNt1/audio.jpg",
-        "url": "https://audio-premium.pl/",
-        "screens": [
-            {
-                "url": "https://i.ibb.co/NLj6f8C/audio.jpg",   
-            },
-            {
-                "url": "https://i.ibb.co/2hHRY2J/audio2.jpg",
-            },
-            {
-                "url": "https://i.ibb.co/S03mcPq/audio3.jpg",
-            },
-        ],
-        "tech":[
-            {
-                "name": "wordpress"
             },
         ],
     },
